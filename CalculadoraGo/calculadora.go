b0c32b18dafb16ced6ebbf9be323077283fa8a34
Noldia Chavez <noldia12@gmail.com>
2020-08-14 20:14:32 -0500

    Se agrego conceptos de condicionales, structs y receivers

--- CalculadoraGo/calculadora.go
+++ CalculadoraGo/calculadora.go
@@ -8,16 +8,45 @@ import (
 	"strings"
 )
 
-func main() {
+type calc struct{}
+
+func (calc) operate(input string, operate string) int {
+	fmt.Println("Resultado: ")
+	cleanInput := strings.Split(input, operate)
+	value1 := changeType(cleanInput[0])
+	value2 := changeType(cleanInput[1])
+	switch operate {
+	case "+":
+		return value1 + value2
+	case "-":
+		return value1 - value2
+	case "*":
+		return value1 * value2
+	case "/":
+		return value1 / value2
+	default:
+		fmt.Println(operate, "No soportado")
+		return 0
+	}
+}
+
+func changeType(input string) int {
+	value, _ := strconv.Atoi(input)
+	return value
+}
+
+func readInput() string {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	operacion := scanner.Text()
-	fmt.Println(operacion)
-	valores := strings.Split(operacion, "+")
-	fmt.Println(valores)
-	fmt.Println(valores[0] + valores[1])
-	operador1, _ := strconv.Atoi(valores[0])
-	operador2, _ := strconv.Atoi(valores[1])
+	return scanner.Text()
+}
 
-	fmt.Println(operador1 + operador2)
+func main() {
+	fmt.Println("-------------Calculadora-----------------")
+	fmt.Println("Ingresa una 2 numero a operar ejm: 2+2 ")
+	input := readInput()
+	fmt.Println("Ahora ingresa el operador ejm: + ")
+	operate := readInput()
+	c := calc{}
+	fmt.Println(c.operate(input, operate))
 }
